32584bcc127950681c704cce31cd8b07d75209b2
Samradnya Thakare <samradnyathakare@gmail.com>
2024-11-18 17:23:52 +0530

    dialog box is added

---
+++ script.js
@@ -0,0 +1,8 @@
+function opendialogBox(){
+  const OverlayElement= document.getElementById('dialogOverlay');
+  OverlayElement.style.display='flex';
+}
+function closedialogBox(){
+  const OverlayElement= document.getElementById('dialogOverlay');
+  OverlayElement.style.display='none';
+}
